28b2cfbc3ae59d1210b6f787179639c7d091b9be
FT-FumihikoUnno <fumihiko.unno@futu-re.co.jp>
2025-12-29 04:46:04 +0000

    ci: add jest-junit reporter and publish JUnit report in CI

--- webapp/src/backend/jest.config.js
+++ webapp/src/backend/jest.config.js
@@ -5,4 +5,8 @@ module.exports = {
   testMatch: ['**/?(*.)+(spec|test).ts'],
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: '<rootDir>/test-results', outputName: 'junit.xml' }]
+  ]
 };
